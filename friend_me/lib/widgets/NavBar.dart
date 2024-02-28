22fd12d3f4a68408e2d1301b81ff1f14c3cb0214
awroda596 <awroda596@gmail.com>
2024-02-28 07:00:13 -0800

    Revert "Update NavBar.dart"
    This reverts commit 6f5eee335cac9b03d0cb2f02660bf92bd4c3847c.

--- friend_me/lib/widgets/NavBar.dart
+++ friend_me/lib/widgets/NavBar.dart
@@ -1,69 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:friend_me/main.dart';
+import 'package:friend_me/pages/profile.dart';
+import 'package:friend_me/pages/schedule.dart';
 
 // Class for the nav bar
-class NavBar extends StatelessWidget implements PreferredSizeWidget{
-     const NavBar({super.key});
-      final textstyle = const TextStyle( // defines text style
-        color: Colors.black,
-        fontSize: 12
-      );
+class NavBar extends StatelessWidget{
+    const NavBar({super.key});
     @override
     Widget build(BuildContext context){ // builds the widget
-        final ButtonStyle outlineButtonStyle = OutlinedButton.styleFrom(
-          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/35  ,vertical: preferredSize.height/10),
-          shape: const LinearBorder(
-            side: BorderSide(color: Colors.black, width: 2),
+        return Drawer( // drawer
+            child:ListView( // list view of the sidbar
+                padding: EdgeInsets.zero, // padding 
+                children: <Widget> [ // children with takes all items on nav bar
+                   const DrawerHeader(  // drawer header
+                        decoration: BoxDecoration( // decoration
+                        color: Colors.grey), // makes color of decoration box grey
+                         child: Text(  // child of decoration with text
+                            'Menu', // menu string
+                            style: TextStyle(color:Colors.white,fontSize:25), // styles the text given color and fontsize
+                        ),
+                    ),  
+                    ListTile(  // home listview on nav bar
+                        title: const Text('Home'), // text of title of item on nav bar
+                         onTap: (){ // on tap
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                          Navigator.push( // push 
+                            context, // context
+                            MaterialPageRoute(builder: (context) => const MyApp()), // push profileroute page
+                          );
+                        }
+                    ),   
+                    ListTile( // list tile for profile
+                        title: const Text('Profile'), // text of title of item on nav bar
+                        onTap: (){ // on tap
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                          Navigator.push( // push 
+                            context, // context
+                            MaterialPageRoute(builder: (context) => const ProfileRoute()), // push profileroute page
+                          );
+                        }
+                    ),     
+                    ListTile( // list tile for profile
+                        title: const Text('Schedule'), // text of title of item on nav bar
+                        onTap: (){ // on tap
+                          Navigator.of(context).pop();
+                          Navigator.of(context).pop();
+                          Navigator.push( // push 
+                            context, // context
+                            MaterialPageRoute(builder: (context) => const ScheduleRoute()), // push profileroute page
+                          );
+                        }
+                    ),   
+                    ListTile(
+                        title: const Text('Friends'), // text of title of item on nav bar
+                        onTap: () => {Navigator.of(context).pop()}, // on tap, use navigation pop // nothing for now
+                    ), 
+                       ListTile(
+                        title: const Text('Settings'), // text of title of item on nav bar
+                        onTap: () => {Navigator.of(context).pop()}, // on tap, use navigation pop // nothing for now
+                    ),  
+            ]
         ));
-        return AppBar( // appbar
-        automaticallyImplyLeading: false, // disables big back button appearing
-        backgroundColor: Colors.grey,  // sets background color
-         title: Column( // coloum 
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [ // children in the column
-            const Text("Friend Me"), // text 
-            Row( // sets row of menu items
-              children: [ // children
-                  OutlinedButton( // home button
-              style: outlineButtonStyle,
-              onPressed: (){
-               Navigator.pop; // pops currnet page
-               Navigator.push( // push new page
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Home')), // temp until home page is seperate
-                  );
-              }, 
-              child: Text("Home", style: textstyle)
-            ),
-             OutlinedButton( // profile button
-              style: outlineButtonStyle,
-               onPressed: (){}, 
-              child:  Text("Profile", style: textstyle)
-            ),
-             OutlinedButton( // Schedule button
-              style: outlineButtonStyle,
-              onPressed: (){}, 
-              child:  Text("Schedule", style: textstyle)
-            ),
-             OutlinedButton( // Friends button
-              style: outlineButtonStyle,
-              onPressed: (){}, 
-              child:  Text("Friends", style: textstyle)
-            ),
-             OutlinedButton( // Settings button
-              style: outlineButtonStyle,
-               onPressed: (){}, 
-              child:  Text("Settings", style: textstyle)
-            ),
-              ],
-            )
-            
-         ],)
-         
-      );
-    } 
-  
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);  // neccessary to return appbar
+    }   
 }
