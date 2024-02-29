e66066744159f68e6b196e7148570275033fa2de
tousif izaz <tousif_izaz@live.com>
2024-02-29 04:47:00 -0800

    created models for events, profiles, schedules. Added rest-framework to applist

--- friend_me_backend/events/models.py
+++ friend_me_backend/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Event(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_events')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
