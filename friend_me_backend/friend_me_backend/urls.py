6895edfca8e079a963023f38f566cbef4984a3ca
tousif izaz <tousif_izaz@live.com>
2024-03-10 20:26:03 -0700

    Added signupview and loginview and tokenized access

--- friend_me_backend/friend_me_backend/urls.py
+++ friend_me_backend/friend_me_backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('profiles.urls')),
+    path('api/profiles/', include('profiles.urls')),
+    path('api/users/', include('users.urls'))
 ]
 
--- friend_me_backend/users/views.py
+++ friend_me_backend/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializers import UserSerializer
 
-# Create your views here.
+class SignupView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = request.data
+        username = data.get('email', None)
+        password = data.get('password', None)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
