576e11554be9f9734c804c0001498c5f9de58798
aroda596 <awroda596@gmail.com>
2024-05-06 09:39:33 -0700

    add respond to FR method
    assuming there will be friend requests to accept/decline, adds a method to send a response to the back end

--- friend_me/lib/widgets/friends_functions.dart
+++ friend_me/lib/widgets/friends_functions.dart
@@ -62,10 +62,10 @@ Future<http.Response> fetchFriends(String? UID) async {
  return response;
 }
 
-//decode response from FetchFriends to retrieve a list of Friends/Users
-List<Friend> listFriends(http.Response response){
+//decode response from FetchFriends or FetchUsers to retrieve a list of Users
+List<User> listUsers(http.Response response){
   Iterable list = json.decode(response.body);
-  List<Friend> Friends =List<Friend>.from(list.map((model) => Friend.fromJson(model)));
+  List<User> Friends =List<User>.from(list.map((model) => User.fromJson(model)));
   return Friends;
 }
 
@@ -99,18 +99,47 @@ Future<http.Response> addFriend(String? UID, int id) async{
   return response; 
 }
 
-//friend class based on users_customuser table.  used for both friends list and user list when searching for friend. 
-class Friend{
+//method to respond to a friend request. 
+Future<http.Response> respondFR(String? UID, int id, String? frResponse) async{
+  String? host = '127.0.0.1'; 
+  int? port = 8000; 
+  String path = 'respondfr'; //placeholder url, replace with backend url. 
+  var url = Uri(
+    scheme: 'http',
+    host: host,
+    port: port,
+    path: path,
+  );
+  http.Response response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': '$UID',
+    },
+    body: jsonEncode(<String, String>{
+      'UID': "$UID", //this user id
+      'id': "$id", // id of a friend request.  
+      'response': "$frResponse",
+    }),
+  );
+  if (response.statusCode != 200 && response.statusCode != 201){
+    return response; 
+  }
+  return response; 
+}
+
+//user class based on users_customuser table.  used for both friends list and user list when searching for friend. 
+class User{
   final int id; //might not be needed
   final String username;
   final String first_name; 
   final String last_name; 
   final String email; 
 
-  Friend({required this.id, required this.username, required this.first_name, required this.last_name, required this.email}); 
+  User({required this.id, required this.username, required this.first_name, required this.last_name, required this.email}); 
 
-  factory Friend.fromJson(Map<String, dynamic> json) {
-    return Friend(
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
       id: json['id'] as int,
       username: json['username'] as String,
       first_name: json['first_name'] as String,
